948d8b691b2410fa71d64b967888aa758da2b073
Saumya.Giri <saumya.giri@automationanywhere.com>
2023-10-11 15:36:18 +0530

    Initial commit

---
+++ src/main/java/com/sam/CoPilotTest2/CoPilotTest2Application.java
@@ -0,0 +1,13 @@
+package com.sam.CoPilotTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoPilotTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoPilotTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/CoPilotTest2/CoPilotTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.CoPilotTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoPilotTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
